13476b17e350963593320996c2499af87c8f0c4d
Co0nmz <99102789+Co0nmz@users.noreply.github.com>
2022-11-07 23:25:38 +0100

    Delete pf1-mini-charactersheet.js

--- pf1-mini-charactersheet.js
+++
@@ -1,7 +0,0 @@
-import { MiniSheet } from './module/sheet.mjs';
-
-Hooks.once('init', () => {
-	Actors.registerSheet('PF1', MiniSheet, { label: 'MiniSheet', types: 'character', makeDefault: false });
-	
-	console.log("PF1 Mini Sheet | loaded");
-});
